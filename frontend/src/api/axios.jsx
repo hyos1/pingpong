4f2ae3cb7ac081a36ca4493343dd41c5bd8ff665
hyos <hyozzang99@gmail.com>
2026-04-23 19:12:40 +0900

    Feat: axios API 요청 시 헤더에 토큰 포함되도록 interceptor 추가

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,8 +1,17 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
-    withCredentials: true,
+  baseURL: "http://localhost:8080/api",
+  withCredentials: true,
+});
+
+// 요청 나가기 전 헤더에 토큰 값 세팅
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers["Authorization"] = token;
+  }
+  return config;
 });
 
 export default api;
