6ccc3d99a48e4cb087a534468850631999e39060
hyos <hyozzang99@gmail.com>
2026-04-24 16:36:49 +0900

    Chore: global is not defined 에러 해결 - Node 전용 객체(global)을 window로 변경

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    // global이 정의되지 않았을 때 window로 대체
+    global: "window",
+  },
+});
