3f97aa57b8b41a9c5ffcfa44a923038bc0917aae
hyos <hyozzang99@gmail.com>
2026-05-02 20:28:11 +0900

    Refactor: User 엔티티에 OAuth 사용자 생성, 업데이트 메서드 추가

--- src/main/java/com/example/pingpong/domain/User.java
+++ src/main/java/com/example/pingpong/domain/User.java
@@ -4,11 +4,13 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class User extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,7 +20,7 @@ public class User extends BaseEntity {
     private String username;
     @Column(nullable = false, unique = true)
     private String email;
-    @Column(nullable = false)
+    @Column
     private String password;
     private String profileImage;
 
@@ -28,7 +30,19 @@ public class User extends BaseEntity {
         this.password = password;
     }
 
+    public void updateOAuthInfo(String name, String profileImage) {
+        this.username = name;
+        this.profileImage = profileImage;
+    }
+
     public static User createUser(String username, String email, String password) {
         return new User(username, email, password);
+    }
+
+    public static User createOAuthUser(String username, String email, String profileImage) {
+        User user = new User(username, email, null);
+        user.profileImage = profileImage;
+
+        return user;
     }
 }
