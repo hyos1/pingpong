f4783189d081883ceb5aa277bf67ff87b2595422
hyos <hyozzang99@gmail.com>
2026-06-16 17:16:35 +0900

    Feat: 존재하지 않는 API 경로 요청, 지원하지 않는 HTTP 메서드 요청 시 처리할 예외 핸들러 추가

--- src/main/java/com/example/pingpong/common/GlobalExceptionHandler.java
+++ src/main/java/com/example/pingpong/common/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.example.pingpong.common;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
@@ -20,6 +23,30 @@ public class GlobalExceptionHandler {
                 .body(failResponse);
     }
 
+    // 지원하지 않는 메서드 요청
+    @ExceptionHandler
+    public ResponseEntity<ApiResponse<Void>> methodNotSupportHandler(
+            HttpRequestMethodNotSupportedException e, HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        String method = request.getMethod();
+        log.warn(requestURI + "에서 " + method + " 메서드를 지원하지 않습니다.");
+        ApiResponse<Void> apiResponse = ApiResponse.fail(method + " 메서드는 지원하지 않습니다.");
+        return ResponseEntity.status(e.getStatusCode()).body(apiResponse);
+    }
+
+    // 존재하지 않는 URI 요청
+    @ExceptionHandler
+    public ResponseEntity<ApiResponse<Void>> noResourceFoundHandler(
+            NoResourceFoundException e, HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        log.warn("존재하지 않는 API 호출 - URI: [{}]", requestURI);
+        ApiResponse<Void> apiResponse = ApiResponse.fail("존재하지 않는 API 경로입니다.");
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(apiResponse);
+    }
+
+    // 처리하지 못한 예외 발생
     @ExceptionHandler
     public ResponseEntity<ApiResponse<Void>> exHandler(Exception e) {
         log.error("서버 예외 발생", e);
