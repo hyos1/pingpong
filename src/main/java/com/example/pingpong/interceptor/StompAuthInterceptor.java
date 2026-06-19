b8d1b81bdca306c91eb269c93949819c6221be7a
hyos <hyozzang99@gmail.com>
2026-06-19 19:17:46 +0900

    Refactor: STOMP 인증 인터셉터 예외 처리 및 에러 응답 기능 추가 - JWT 검증 시 발생할 수 있는 다양한 예외 캐치 - 발생한 예외 정보 STOMP ERROR 프레임에 담아 전달하는 메서드 추가

--- src/main/java/com/example/pingpong/interceptor/StompAuthInterceptor.java
+++ src/main/java/com/example/pingpong/interceptor/StompAuthInterceptor.java
@@ -1,12 +1,14 @@
 package com.example.pingpong.interceptor;
 
 import com.example.pingpong.common.ClientException;
-import com.example.pingpong.common.ErrorCode;
 import com.example.pingpong.config.JwtAuthenticationToken;
 import com.example.pingpong.config.JwtUtil;
 import com.example.pingpong.enums.UserRole;
 import com.example.pingpong.web.dto.AuthUser;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
@@ -14,11 +16,12 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import java.security.Principal;
+import static com.example.pingpong.common.ErrorCode.*;
 
 @Slf4j
 @Component
@@ -36,23 +39,48 @@ public class StompAuthInterceptor implements ChannelInterceptor {
 
         // 첫 연결(CONNECT)일 때만 실행
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-            // 헤더에서 토큰 값 추출 "Bearer adfasdfa.asdfasdf.sadf"
+            // Authorization헤더 배열의 여러 값 중 첫 번째 값"
             String bearerToken = accessor.getFirstNativeHeader("Authorization");
 
-            if (bearerToken == null || !bearerToken.startsWith("Bearer ")) {
-                throw new ClientException(ErrorCode.TOKEN_NOT_FOUND);
+            try {
+                // "Bearer "제거
+                String token = jwtUtil.subStringToken(bearerToken); // 예외 발생 가능
+
+                // JWT 만료, 변조 검사 시 관련 예외 발생 가능
+                Claims claims = jwtUtil.extractClaims(token);
+                log.info("STOMP Jwt Claims 파싱 성공: {}", claims);
+
+                Long userId = Long.valueOf(claims.getSubject());
+                String email = claims.get("email", String.class);
+                UserRole userRole = UserRole.of(claims.get("userRole", String.class));
+
+                AuthUser authUser = new AuthUser(userId, email, userRole);
+                Authentication authentication = new JwtAuthenticationToken(authUser);
+                accessor.setUser(authentication);
+
+            } catch (ExpiredJwtException e) {
+                log.info("[STOMP JWT 만료]: userId={}", e.getClaims().getSubject());
+                return createErrorMessage(accessor, TOKEN_EXPIRED.name(), TOKEN_EXPIRED.getMessage());
+            } catch (ClientException e) {
+                log.warn("[STOMP 인증 실패] 토큰 형식 문제 발생: {}", e.getMessage());
+                return createErrorMessage(accessor, e.getErrorCode().name(), e.getMessage());
+            } catch (SecurityException | MalformedJwtException | UnsupportedJwtException e) {
+                log.error("[STOMP JWT 검증 실패] [{}]: ", e.getClass().getSimpleName(), e);
+                return createErrorMessage(accessor, INVALID_TOKEN.name(), INVALID_TOKEN.getMessage());
+            } catch (Exception e) {
+                log.error("[STOMP 인증 실패] 알 수 없는 시스템 오류 발생: ", e);
+                return createErrorMessage(accessor, INTERNAL_SERVER_ERROR.name(), INVALID_TOKEN.getMessage());
             }
-            // "Bearer "제거
-            String token = jwtUtil.subStringToken(bearerToken);
-            Claims claims = jwtUtil.extractClaims(token);
-            Long userId = Long.valueOf(claims.getSubject());
-            String email = claims.get("email", String.class);
-
-            AuthUser authUser = new AuthUser(userId, email, UserRole.ROLE_USER);
-            Authentication authentication = new JwtAuthenticationToken(authUser);
-
-            accessor.setUser((Principal) authentication.getPrincipal());
         }
         return message;
+    }
+
+    private Message<byte[]> createErrorMessage(StompHeaderAccessor accessor, String errorCode, String message) {
+        StompHeaderAccessor errorAccessor = StompHeaderAccessor.create(StompCommand.ERROR);
+        errorAccessor.setMessage(message);
+        errorAccessor.setHeader("errorCode", errorCode);
+        errorAccessor.setSessionId(accessor.getSessionId());
+        errorAccessor.setSessionAttributes(accessor.getSessionAttributes());
+        return MessageBuilder.createMessage(new byte[0], errorAccessor.getMessageHeaders());
     }
 }
